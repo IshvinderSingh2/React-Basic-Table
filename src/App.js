c6a341f0fdd30c1c707f7902ec14659ac899f9ff
Ishvinder Singh <120705961+IshvinderSingh2@users.noreply.github.com>
2023-01-13 14:22:44 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import BasicTable from "./components/BasicTable";
+export default function App() {
+  return (
+    <>
+      <h2 className="mt-3 container center">The Basic Simple React Table</h2>
+      <BasicTable />
+    </>
+  );
+}
